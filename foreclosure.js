2f79f9d9667f8762f13c6d9a5c161a1fcaf002ff
BK <bkam03@yahoo.com>
2017-05-18 20:24:35 -1000

    borrower function created and added to steve

--- foreclosure.js
+++ foreclosure.js
@@ -35,9 +35,38 @@ function loan(){
   };
 }
 
+function borrower(loan) {
+  //prV
+  var account = {
+    monthlyIncome : 1350,
+    funds : 2800,
+    loan : loan
+  };
+  //puM
+  var getFunds = function(){
+    return account.funds;
+  };
+  var makePayment = function(){
+    if(account.funds > loan.monthlyPayment){
+      account.fund -= loan.monthlyPayment;
+    } else{
+      loan.receivePayment(account.funds);
+    }
+  };
+  var payDay = function(){
+    account.funds += account.monthlyIncome;
+  };
+  return {
+    getFunds : getFunds,
+    makePayment : makePayment,
+    payDay : payDay
+  };
+}
+
 var steve;
 var stevesLoan;
 var month = 0;
 var monthsUntilEvicted;
 
 stevesLoan = loan();
+steve = borrower();
