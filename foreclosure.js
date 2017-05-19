5f3c9af432688c9ac55d1a05b03287f290a30434
BK <bkam03@yahoo.com>
2017-05-18 19:51:33 -1000

    loan function sans return

--- foreclosure.js
+++ foreclosure.js
@@ -1,6 +1,30 @@
 'use strict';
 
+function loan(){
+  var account = {
+    borrowed : 550000,
+    balance : 286000,
+    monthlyPayment : 1700,
+    defaulted : 0,
+    defaultedToForeclose : 5,
+    foreclosed : false
+  };
+  var missPayment = function(){
+    var current = account.defaulted++;
+    if(current >= account.defaultedToForeclose) {
+      account.foreclosed = true;
+    }
+  };
+  return {
+    getBalance : function(){
+      return balance;
+    }
+  };
+}
+
 var steve;
 var stevesLoan;
 var month = 0;
 var monthsUntilEvicted;
+
+stevesLoan = loan();
